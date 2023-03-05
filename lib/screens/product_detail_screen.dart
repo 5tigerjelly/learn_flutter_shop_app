bc52a0951002efa793f4f4f919a4943986c6952f
Chris Oh <chris@5tigerjelly.com>
2023-03-05 16:10:22 -0500

    able to access product data from provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -9,9 +11,14 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final product = Provider.of<Products>(
+      context,
+      // This listen is false if you don't want to reload after build run
+      listen: false,
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(product.title),
       ),
     );
   }
