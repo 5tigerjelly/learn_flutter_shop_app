279e789550a39816423b9e0523f3b8df594c770c
Chris Oh <chris@5tigerjelly.com>
2023-03-05 15:56:19 -0500

    add provider package. Add products provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    final products = productData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+      itemCount: products.length,
+    );
+  }
+}
