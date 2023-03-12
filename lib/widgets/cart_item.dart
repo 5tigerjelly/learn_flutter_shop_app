7590dfa50abcd28775c0d3c8480d4209a3607b01
Chris Oh <chris@5tigerjelly.com>
2023-03-11 23:57:24 -0500

    cart item is dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -13,22 +13,26 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text('\$$price'),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(color: Theme.of(context).errorColor),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$$price'),
+                ),
               ),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity'),
         ),
       ),
     );
